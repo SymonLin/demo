3f6095c686d2bde5b831d116b8d495d6cb7cbb88
林剑 <1289804434@qq.com>
2019-01-28 16:08:48 +0800

    添加单元测试

---
+++ demo-web/src/test/com/example/demo/biz/service/DemoServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.biz.service;
+
+import com.example.demo.web.DemoWebApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author linjian
+ * @date 2019/1/28
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {DemoWebApplication.class})
+public class DemoServiceTest {
+
+    @Autowired
+    private DemoService demoService;
+
+    @Test
+    public void test() {
+        System.out.println(demoService.test());
+    }
+}
