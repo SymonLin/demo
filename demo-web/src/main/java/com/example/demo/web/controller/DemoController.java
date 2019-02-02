5296238bc8c26086c7c00948747ac3ee8045f7a5
林剑 <1289804434@qq.com>
2019-02-02 17:02:35 +0800

    集成 swagger

--- demo-web/src/main/java/com/example/demo/web/controller/DemoController.java
+++ demo-web/src/main/java/com/example/demo/web/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.example.demo.web.controller;
 
 import com.example.demo.biz.service.DemoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author linjian
  * @date 2019/1/15
  */
+@Api(tags = "demo")
 @RestController
 @RequestMapping("demo")
 public class DemoController {
@@ -18,6 +21,7 @@ public class DemoController {
     private DemoService demoService;
 
     @GetMapping("test")
+    @ApiOperation("测试")
     public String test() {
         return demoService.test();
     }
