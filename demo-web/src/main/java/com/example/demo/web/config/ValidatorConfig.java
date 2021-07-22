b41d9453d3a8c0242694fae677e2266f6acb9bdb
林剑 <linjian.lj@120yibao.com>
2021-07-22 12:16:16 +0800

    feat: 集成validator参数校验

---
+++ demo-web/src/main/java/com/example/demo/web/config/ValidatorConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.web.config;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+/**
+ * @author linjian
+ * @date 2021/3/17
+ */
+@Configuration
+public class ValidatorConfig {
+
+    @Bean
+    public Validator validator() {
+        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                .failFast(true)
+                .buildValidatorFactory();
+        return validatorFactory.getValidator();
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        MethodValidationPostProcessor methodValidationPostProcessor = new MethodValidationPostProcessor();
+        methodValidationPostProcessor.setValidator(validator());
+        return methodValidationPostProcessor;
+    }
+}
